edb544924e315fe8c86b679dfbce8304f80bb3a8
kiki <2958552941@qq.com>
2023-09-27 21:50:48 +0800

    ...

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,8 @@
 module.exports = {
-    host: '0.0.0.0', // ip
-    port: '8099', //端口号
-    title: '我的技术站', // 设置网站标题
-    description: '描述：我的技术站'
+    title: '我的技术站',
+    description: '描述：我的技术站',
+    base: '/',
+    host: '0.0.0.0',
+    port: '8099',
+    dest: './dist',  // 打包文件夹
 }
